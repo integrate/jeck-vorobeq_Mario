e6188782249bbba07a7edeef8a651e6e893858c1
jeck-vorobeq <m.a.karasev2011@gmale.com>
2021-12-21 10:04:10 +0300

    it is  ME MARIO and LUIGE

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import wrap,object_physics
 wrap.world.create_world(1900,500)
 wrap.world.set_back_color(45,81,253)
 wrap.sprite.add("mario-scenery",100,480,"block")
-mario=object_physics.create_physics("mario-1-small", 100, 0, "stand")
-lue=object_physics.create_physics("mario-2-small", 200, 0, "stand")
+mario=object_physics.create_physics("mario-1-small", 100, 0, "stand",3)
+lue=object_physics.create_physics("mario-2-small", 200, 0, "stand",0)
 
 @wrap.always
 def phisics():
--- object_physics.py
+++ object_physics.py
@@ -3,12 +3,13 @@ import random
 import wrap
 
 
-def create_physics(sprite,x,y,costum):
+def create_physics(sprite,x,y,costum,speed):
     marIo=wrap.sprite.add(sprite,x,y,costum)
-    a={"ID":marIo,"speed":random.randint(1,5)}
+    a={"ID":marIo,"speed":speed}
 
     return a
 
 
 def power_gravity(b):
-    wrap.sprite.move(b["ID"],0, bl["speed"])
+    wrap.sprite.move(b["ID"],0, b["speed"])
+    b["speed"]+=1
