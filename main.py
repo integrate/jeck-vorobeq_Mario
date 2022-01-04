3ee11a666d05e94ad11bd0679884e5163cd05c19
jeck-vorobeq <m.a.karasev2011@gmale.com>
2022-01-04 13:08:25 +0300

    it is  ME MARIO and LUIGE

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-import wrap,object_physics
+import wrap, object_physics
 
-wrap.world.create_world(1900,500)
-wrap.world.set_back_color(45,81,253)
-wrap.sprite.add("mario-scenery",100,480,"block")
-mario=object_physics.create_physics("mario-1-small", 100, 0, "stand",3)
-lue=object_physics.create_physics("mario-2-small", 200, 0, "stand",0)
+wrap.world.create_world(1900, 500)
+wrap.world.set_back_color(45, 81, 253)
+id=wrap.sprite.add("mario-scenery", 100, 480, "block")
+mario = object_physics.create_physics("mario-1-small", 100, 200, "stand", 3)
+lue = object_physics.create_physics("mario-2-small", 100, 0, "stand", 0)
+
 
 @wrap.always
-def phisics():
-    y = wrap.sprite.get_y(lue["ID"])
-    x = wrap.sprite.get_x(lue["ID"])
-    object_physics.power_gravity(lue,y,500)
-    y1 = wrap.sprite.get_y(mario["ID"])
-    x1 = wrap.sprite.get_x(mario["ID"])
-    object_physics.power_gravity(mario,y1,x1)
+def run_phisics():
+    object_physics.power_gravity(mario,wrap.sprite.get_top(id))
+    object_physics.power_gravity(lue,wrap.sprite.get_top(mario["ID"]))
--- object_physics.py
+++ object_physics.py
@@ -3,18 +3,18 @@ import random
 import wrap
 
 
-def create_physics(sprite,x,y,costum,speed):
-    marIo=wrap.sprite.add(sprite,x,y,costum)
-    a={"ID":marIo,"speed":speed}
+def create_physics(sprite, x, y, costum, speed):
+    marIo = wrap.sprite.add(sprite, x, y, costum)
+    a = {"ID": marIo, "speed": speed}
 
     return a
 
 
-def power_gravity(b,y,x):
-    landing=480
-    wrap.sprite.move(b["ID"],0, b["speed"])
-    b["speed"]+=1
-    #если y марио 480 или больше то телепорт. на 480
+def power_gravity(b, landing):
+    wrap.sprite.move(b["ID"], 0, b["speed"])
+    b["speed"] += 1
+    bottom = wrap.sprite.get_bottom(b["ID"])
+    # если y ОБЪЕКТА 480 или больше то телепорт. на 480
 
-    if y >= 480:
-        wrap.sprite.move_to(b["ID"],x,480)
+    if bottom >= landing:
+        wrap.sprite.move_bottom_to(b["ID"],landing)
