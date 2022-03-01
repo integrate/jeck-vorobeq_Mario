6e5f501162f55e2c6d7f311aca16cb32dbfd2346
jeck-vorobeq <m.a.karasev2011@gmale.com>
2022-03-01 10:02:35 +0300

    it is  ME MARIO and LUIGE

--- main.py
+++ main.py
@@ -6,36 +6,38 @@ id = wrap.sprite.add("mario-scenery", 100, 380, "block")
 cloud_platform = wrap.sprite.add("mario-items", 500, 200, "cloud_platform")
 cloud_platform2 = wrap.sprite.add("mario-items", 600, 300, "cloud_platform")
 cloud_platform3 = wrap.sprite.add("mario-items", 400, 100, "cloud_platform")
-mario = object_physics.create_physics("mario-1-small", mario_mod, 550, 200, "stand", -8)
-lue = object_physics.create_physics("mario-enemies", dracon, 100, 0, "dragon_stand1", 0)
+mario = object_physics.create_physics("mario-1-small", mario_mod, 550, 200, "stand", -8,200)
+#lue = object_physics.create_physics("mario-enemies", dracon, 100, 0, "dragon_stand1", 0,wrap.sprite.get_top(id))
+lue = object_physics.create_physics("mario-enemies", dracon, 100, 0, "dragon_stand1", 0,300)
 grounds_list=[cloud_platform ,cloud_platform2,cloud_platform3]
 
 @wrap.always
 def run_phisics():
-    object_physics.power_gravity(mario, wrap.sprite.get_top(id))
-    object_physics.power_gravity(lue, wrap.sprite.get_top(id))
+    object_physics.power_gravity(mario)
+    mario["landing"] = look_earth()
+    object_physics.power_gravity(lue)
 
 
 @wrap.on_key_down(wrap.K_w)
 def jump():
-    object_physics.jump(mario, wrap.sprite.get_top(id))
+    object_physics.jump(mario)
 
 
 @wrap.on_key_down(wrap.K_UP)
 def jump():
-    object_physics.jump(lue, wrap.sprite.get_top(id))
+    object_physics.jump(lue)
 
 
 @wrap.on_key_always(wrap.K_a)
 def move():
     object_physics.move_left(mario)
-    look_earth()
+    mario["landing"]=look_earth()
 
 
 @wrap.on_key_always(wrap.K_d)
 def move():
     object_physics.move_right(mario)
-    look_earth()
+    mario["landing"]=look_earth()
 
 
 def look_earth():
@@ -72,6 +74,8 @@ def look_earth():
     else:
         max=500
     print(max)
+
+    return max
 # хотели Далее: переписать условие удаления островов, которые выше
 
 @wrap.on_key_always(wrap.K_LEFT)
--- object_physics.py
+++ object_physics.py
@@ -3,32 +3,33 @@ import random
 import wrap
 
 
-def create_physics(sprite,mod, x, y, costum, speed):
+def create_physics(sprite,mod, x, y, costum, speed,landing):
     marIo = wrap.sprite.add(sprite, x, y, costum)
-    a = {"ID": marIo,"MOD":mod, "speed": speed}
+    a = {"ID": marIo,"MOD":mod, "speed": speed,"landing":landing}
 
     return a
 
 
-def power_gravity(b, landing):
+def power_gravity(b):
     wrap.sprite.move(b["ID"], 0, b["speed"])
     bottom = wrap.sprite.get_bottom(b["ID"])
-    if bottom < landing:
+    if bottom < b["landing"]:
         b["speed"] += 1
 
     # если y ОБЪЕКТА 480 или больше то телепорт. на 480
 
-    if bottom > landing:
+    if bottom > b["landing"]:
         b["speed"] = 0
-        wrap.sprite.move_bottom_to(b["ID"],landing)
+        wrap.sprite.move_bottom_to(b["ID"],b["landing"])
         b["MOD"].costum_stand(b)
 
 
-def jump(b,landing):
+def jump(b):
     y_landing=wrap.sprite.get_bottom(b["ID"])
-    b["MOD"].costum_jump(b)
-    if y_landing ==landing:
+    if y_landing ==b["landing"]:
         b["speed"] = -20
+        b["MOD"].costum_jump(b)
+
 
 def move_left(b):
     wrap.sprite.move(b["ID"],-5,0)
