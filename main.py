a4252405afc30cfdfcb9676c4e5fceee6c50450b
jeck-vorobeq <m.a.karasev2011@gmale.com>
2022-03-29 10:05:22 +0300

    it is  ME MARIO and LUIGE koteika

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import wrap
+import wrap,random
 wrap.add_sprite_dir("img_1")
 wrap.world.create_world(1350, 500)
 
@@ -12,14 +12,32 @@ grounds_list=[cloud_platform ,cloud_platform2,cloud_platform3,id]
 mario = object_physics.create_physics("mario-1-small", mario_mod, 400, 50, "stand", -8,200,grounds_list)
 #lue = object_physics.create_physics("mario-enemies", dracon, 100, 0, "dragon_stand1", 0,wrap.sprite.get_top(id))
 lue = object_physics.create_physics("mario-enemies", dracon, 100, 0, "dragon_stand1", 0,300,grounds_list)
+vode=None
+@wrap.on_key_down(wrap.K_1)
+def odin():
+    global vode
+    vode=1
+
+
+@wrap.on_key_down(wrap.K_2)
+def dva():
+    global vode
+    vode=2
+
+@wrap.always()
+def voda():
+    if vode == 1:
+        STRELKA.ykaz_strelk(mario)
+    else:
+        STRELKA.ykaz_strelk(lue)
+
 
 @wrap.always
 def run_phisics():
     object_physics.power_gravity(mario)
 
     object_physics.power_gravity(lue)
-    STRELKA.ykaz_strelk(mario)
-    STRELKA.ykaz_strelk(lue)
+
 @wrap.on_key_down(wrap.K_w)
 def jump():
     object_physics.jump(mario)
@@ -36,40 +54,24 @@ def move():
 
     object_physics.move_left(mario)
 
-    STRELKA.ykaz_strelk(mario)
+
 
 
 @wrap.on_key_always(wrap.K_d)
 def move():
     object_physics.move_right(mario)
-    STRELKA.ykaz_strelk(mario)
+
+@wrap.on_key_always(wrap.K_LEFT)
+def move():
+
+    object_physics.move_left(lue)
 
 
-@wrap.on_key_down(wrap.K_1)
-def odin():
-    STRELKA.ykaz_strelk(mario)
-
-    @wrap.on_key_always(wrap.K_a)
-    def move():
-        object_physics.move_left(mario)
-        STRELKA.ykaz_strelk(mario)
-
-    @wrap.on_key_always(wrap.K_d)
-    def move():
-        object_physics.move_right(mario)
-        STRELKA.ykaz_strelk(mario)
 
 
-@wrap.on_key_down(wrap.K_2)
-def dva():
-    STRELKA.ykaz_strelk(lue)
+@wrap.on_key_always(wrap.K_RIGHT)
+def move():
+    object_physics.move_right(lue)
 
-    @wrap.on_key_always(wrap.K_LEFT)
-    def move():
-        object_physics.move_left(lue)
-        STRELKA.ykaz_strelk(lue)
 
-    @wrap.on_key_always(wrap.K_RIGHT)
-    def move():
-        object_physics.move_right(lue)
-        STRELKA.ykaz_strelk(lue)
+
