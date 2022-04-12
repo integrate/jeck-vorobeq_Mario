b381dc1551ae1fff99c09573631bd2864eeb48ef
jeck-vorobeq <m.a.karasev2011@gmale.com>
2022-04-12 10:08:45 +0300

    it is  ME MARIO and LUIGE koteika

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ wrap.world.create_world(1350, 500)
 
 import object_physics, mario as mario_mod, dracon,STRELKA
 wrap.world.set_back_image("img.png")
-seconds=60
+seconds=65
 seconde=seconds-1
 schet=wrap.sprite.add_text(str(seconde), 600, 450, text_color=[255, 255, 255], font_size=50)
 id = wrap.sprite.add("mario-scenery", 100, 380, "block")
@@ -15,25 +15,20 @@ cloud_platform3 = wrap.sprite.add("mario-items", 400, 100, "cloud_platform")
 cloud_platform4 = wrap.sprite.add("mario-items", 1000, 200, "cloud_platform")
 cloud_platform5 = wrap.sprite.add("mario-items", 1200, 300, "cloud_platform")
 cloud_platform6 = wrap.sprite.add("mario-items", 600, 100, "cloud_platform")
+death=wrap.sprite.add("img_1",100,200,"img")
 grounds_list=[cloud_platform ,cloud_platform2,cloud_platform3,id,id1,cloud_platform4 ,cloud_platform5,cloud_platform6]
 mario = object_physics.create_physics("mario-1-small", mario_mod, 400, 50, "stand", -8,200,grounds_list)
 #lue = object_physics.create_physics("mario-enemies", dracon, 100, 0, "dragon_stand1", 0,wrap.sprite.get_top(id))
 lue = object_physics.create_physics("mario-enemies", dracon, 100, 0, "dragon_stand1", 0,300,grounds_list)
 vode=None
 
-def odin():
-    global vode
-    vode=1
 
-
-
-def dva():
-    global vode
-    vode=2
 
 
 def voda():
+
     if vode == 1:
+
         STRELKA.ykaz_strelk(mario)
     else:
         STRELKA.ykaz_strelk(lue)
@@ -83,12 +78,14 @@ def move():
 
 @wrap.always
 def pered_str():
-    global vode
+    global vode,seconde
     collide_strelka = wrap.sprite.is_collide_sprite(lue["ID"], mario["ID"])
     if vode==1 and collide_strelka==True:
+        seconde = 65
         vode=2
         wrap.sprite.move_to(lue["ID"], 1100, -1600)
     elif collide_strelka == True:
+        seconde=65
         vode=1
         wrap.sprite.move_to(mario["ID"],100,-1600)
 @wrap.always(1000)
